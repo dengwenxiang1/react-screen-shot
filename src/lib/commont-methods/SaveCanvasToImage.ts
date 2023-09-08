eb42874448f822ecce1ef5026c2b5c2f71312b73
wenxiang <qq.com>
2023-09-08 15:53:24 +0800

    定义获取裁剪框数据类

--- src/lib/commont-methods/SaveCanvasToImage.ts
+++ src/lib/commont-methods/SaveCanvasToImage.ts
@@ -1,4 +1,33 @@
-import {getCanvas2dCtx} from "@/lib/commont-methods/CanvasPatch
-export function saveCanvasToImage (){
+import {getCanvas2dCtx} from "@/lib/commont-methods/CanvasPatch"
+export function saveCanvasToImage (
+    context:CanvasRenderingContext2D,
+    startX:number,
+    startY:number,
+    width:number,
+    height:number
+){
+  //  获取设备像素比
+  const dpr =window.devicePixelRatio ||1;
+  //  获取裁剪框区域图片信息
+  const img = context.getImageData(
+    startY *dpr,
+    startX * dpr,
+    width *dpr,
+    height *dpr
+  );
+  //  创建canvas标签，用于存放裁剪区域的图片
+  const canvas = document.createElement("canvas");
+  //  获取裁剪区域画布
+  const imgContext =getCanvas2dCtx(canvas,width,height);
+  if(imgContext){
+    //  将图片放进裁剪框内
+    imgContext.putImageData(img,0,0);
+    const a =document.createElement("a")
+    //  获取图片
+    a.href=canvas.toDataURL("png");
 
+    //下载图片
+     a.download=`${new Date().getTime()}.png`
+     a.click();
+  }
 }
