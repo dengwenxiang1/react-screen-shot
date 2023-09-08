f91e7afc380b65c487ac0a6a69689f6f602aa8a8
wenxiang <qq.com>
2023-09-08 16:48:31 +0800

    定义将裁剪框数据转为base64格式

--- src/lib/commont-methods/GetCanvasImgData.ts
+++ src/lib/commont-methods/GetCanvasImgData.ts
@@ -1,4 +1,5 @@
 import { saveCanvasToImage} from "@/lib/commont-methods/SaveCanvasToImage"
+import {saveCanvasToBase64} from "@/lib/commont-methods/SaveCanvasToBase64"
 /**
  * 将指定区域的canvas转为图片
  */
