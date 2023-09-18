e2f606561a08fbda01fcbcea9fc3312469d1bba5
wenxiang <qq.com>
2023-09-18 20:42:56 +0800

    定义绘制图片函数

--- src/main.ts
+++ src/main.ts
@@ -28,4 +28,5 @@ import html2canvas from "html2canvas";
 import PlugInParameters from "@/lib/main-entrance/PlugInParameters";
 import {getDrawBoundaryStatus} from "@/lib/split-methods/BoundaryJudgment";
 import  KeyboardEventHandle from "@/lib/split-methods/KeyboardEventHandle";
-import {setPlugInParameters} from "@/lib/split-methods/SetPlugInParameters"
+import {setPlugInParameters} from "@/lib/split-methods/SetPlugInParameters";
+import {drawCrossImg} from "@/lib/split-methods/drawCrossImg";
